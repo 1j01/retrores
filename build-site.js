701223efeed30f83909a2a26ad1f660d3a5b71bc
Isaiah Odhner <isaiahodhner@gmail.com>
2018-04-18 00:15:34 -0400

    Prettify HTML

--- build-site.js
+++ build-site.js
@@ -1,6 +1,7 @@
 
 var fs = require("fs");
 var slug = require("slug");
+var beautify_html = require("js-beautify").html;
 var Template = require("template-js");
 var resources = require("./catalog.js");
 
@@ -25,9 +26,11 @@ resources.forEach(function (resource) {
 });
 resources.forEach(function (resource) {
 	var html_file_path = resource.__htmlPath;
-	var html_content = new Template("templates/resource-page-template.html", resource);
+	var html_content = new Template("templates/resource-page-template.html", resource).toString();
+	html_content = beautify_html(html_content, {indent_with_tabs: true});
 	fs.writeFileSync(require("path").join(site_folder, html_file_path), html_content);
 });
 
-var homepage_html = new Template("templates/homepage-template.html", {resources});
+var homepage_html = new Template("templates/homepage-template.html", {resources}).toString();
+homepage_html = beautify_html(homepage_html, {indent_with_tabs: true});
 fs.writeFileSync(require("path").join(site_folder, "index.html"), homepage_html);
