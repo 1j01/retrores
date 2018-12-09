d6ad8f8890e80277f77e6b979b6299dcef184f9b
Isaiah Odhner <isaiahodhner@gmail.com>
2018-12-09 13:18:12 -0500

    Progress

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const withPreact = require('@zeit/next-preact')
+const resources = require('./catalog.js')
 
 module.exports = withPreact({
 	exportPathMap: function () {
--- server.js
+++ server.js
@@ -1,16 +1,37 @@
 require('@zeit/next-preact/alias')()
 const { createServer } = require('http')
+// const express = require('express')
 const next = require('next')
+const resources = require('./catalog.js')
 
-
-const app = next({ dev: process.env.NODE_ENV !== 'production' })
+const dev = process.env.NODE_ENV !== 'production'
 const port = process.env.PORT || 3000
+
+const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare()
 .then(() => {
-  createServer(handle)
-  .listen(port, () => {
-    console.log(`> Ready on http://localhost:${port}`)
-  })
+	createServer(handle)
+	.listen(port, () => {
+		console.log(`> Ready on http://localhost:${port}`)
+	})
 })
+
+// app.prepare()
+// .then(() => {
+// 	const server = express()
+
+// 	server.get('*', (req, res) => {
+// 		return handle(req, res)
+// 	})
+
+// 	server.listen(3000, (err) => {
+// 		if (err) throw err
+// 		console.log('> Ready on http://localhost:3000')
+// 	})
+// })
+// .catch((ex) => {
+// 	console.error(ex.stack)
+// 	process.exit(1)
+// })
