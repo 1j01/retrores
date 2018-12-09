407b4658dafadfdc361f00f37aba41c953d51733
Isaiah Odhner <isaiahodhner@gmail.com>
2018-12-09 14:41:14 -0500

    Hm? Is the server using exportPathMap???

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('@zeit/next-preact/alias')()
-// const { createServer } = require('http')
-const express = require('express')
+const { createServer } = require('http')
+// const express = require('express')
 const next = require('next')
 const resources = require('./catalog.js')
 // TODO: hot-reload resources list?
@@ -13,22 +13,27 @@ const handle = app.getRequestHandler()
 
 app.prepare()
 .then(() => {
-	const server = express()
-
-	// server.get('/:id/:name-slug', (req, res) => {
-	// 	const actualPage = '/resource-page'
-	// 	const queryParams = { id: req.params.id } 
-	// 	app.render(req, res, actualPage, queryParams)
-	// })
-
-	server.get('*', (request, response) => {
-		return handle(request, response)
-	})
-
-	server.listen(port, (error) => {
-		if (error) throw error
+	createServer(handle)
+	.listen(port, () => {
 		console.log(`> Ready on http://localhost:${port}`)
 	})
+
+	// const server = express()
+
+	// // server.get('/:id/:name-slug', (req, res) => {
+	// // 	const actualPage = '/resource-page'
+	// // 	const queryParams = { id: req.params.id } 
+	// // 	app.render(req, res, actualPage, queryParams)
+	// // })
+
+	// server.get('*', (request, response) => {
+	// 	return handle(request, response)
+	// })
+
+	// server.listen(port, (error) => {
+	// 	if (error) throw error
+	// 	console.log(`> Ready on http://localhost:${port}`)
+	// })
 })
 .catch((error) => {
 	console.error(error.stack)
